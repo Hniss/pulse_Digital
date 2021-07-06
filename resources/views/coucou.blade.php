10134dac9644214221f629ad83eb2939f3de1867
Hamza <nissoul.hamza@gmail.com>
2021-07-06 14:30:12 +0100

    testing auto synchronisation with server (third time)

--- resources/views/coucou.blade.php
+++ resources/views/coucou.blade.php
@@ -1,6 +1,6 @@
 <h3>This is first synchronization test from git to server</h3>
 <h3>This is seconde synchronization test from git to server</h3>
-<h4>this is third test </h4>
+<h4>this is third test - v2</h4>
 
 {{ $message }}
 
